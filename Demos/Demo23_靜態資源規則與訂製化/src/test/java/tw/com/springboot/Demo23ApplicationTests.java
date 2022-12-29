5f778b001376cfa2f1fdb9b0ac10615414625324
eason <regretyou710@gmail.com>
2022-12-29 20:27:13 +0800

    202212292026

---
+++ Demos/Demo23_靜態資源規則與訂製化/src/test/java/tw/com/springboot/Demo23ApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
