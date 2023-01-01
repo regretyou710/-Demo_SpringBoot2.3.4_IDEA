e2ce21d266fe8ecf696ca7e00d772addc6fe7be6
eason <regretyou710@gmail.com>
2023-01-01 10:32:44 +0800

    202301011023

---
+++ Demos/Demo50_單文件與多文件的上傳使用/src/main/java/tw/com/springboot/bean/User.java
@@ -0,0 +1,15 @@
+package tw.com.springboot.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String userName;
+    private String password;
+}
